d14b76cee1f0b6b5e5419c9aa9a0b23a10ff840f
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-26 16:51:13 -0500

    Repo inicial

---
+++ demopredojo/demopredojo/src/main/java/com/sofkau/demopredojo/PredojoreactivofuncionalApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.demopredojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredojoreactivofuncionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredojoreactivofuncionalApplication.class, args);
+	}
+
+}
